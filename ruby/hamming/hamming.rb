3ad90a5f70c8131dfc499ec4e2440f246d13f4be
Taylor Moore <Tmoore2272@gmail.com>
2015-11-05 21:39:50 -0700

    iteration 2

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -4,12 +4,10 @@ class Hamming
   def self.compute(string, string2)
     raise ArgumentError if string.length != string2.length
 
-    counter = 0
     array = string.chars.zip(string2.chars)
 
-    array.map do |pair1, pair2|
-      counter += 1 if pair1 != pair2
+    array.count do |pair1, pair2|
+      pair1 != pair2
     end
-    counter
   end
 end
